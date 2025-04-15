8afa6e1335ade4d67e621c676b401824d6869f73
Alfred Mah <alfredmjz@gmail.com>
2025-04-15 11:56:06 -0400

    Pick up variables from global.css

--- main/tailwind.config.ts
+++ main/tailwind.config.ts
@@ -1,48 +1,52 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: ['class'],
-	content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
+	content: ['app/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}', 'styles/**/*.css'],
 	theme: {
 		extend: {
 			colors: {
-				border: 'hsl(var(--border))',
-				input: 'hsl(var(--input))',
-				ring: 'hsl(var(--ring))',
-				background: 'hsl(var(--background))',
-				foreground: 'hsl(var(--foreground))',
+				ring: 'var(--ring)',
+				background: 'var(--background)',
 				primary: {
-					DEFAULT: 'hsl(var(--primary))',
-					foreground: 'hsl(var(--primary-foreground))',
+					DEFAULT: 'var(--primary)',
+					highlight: 'var(--primary-highlight)',
+					muted: 'var(--primary-muted)',
 				},
 				secondary: {
-					DEFAULT: 'hsl(var(--secondary))',
-					foreground: 'hsl(var(--secondary-foreground))',
-				},
-				destructive: {
-					DEFAULT: 'hsl(var(--destructive))',
-					foreground: 'hsl(var(--destructive-foreground))',
-				},
-				muted: {
-					DEFAULT: 'hsl(var(--muted))',
-					foreground: 'hsl(var(--muted-foreground))',
+					DEFAULT: 'var(--secondary)',
+					highlight: 'var(--secondary-highlight)',
+					muted: 'var(--secondary-muted)',
 				},
 				accent: {
-					DEFAULT: 'hsl(var(--accent))',
-					foreground: 'hsl(var(--accent-foreground))',
+					DEFAULT: 'var(--secondary)',
 				},
-				popover: {
-					DEFAULT: 'hsl(var(--popover))',
-					foreground: 'hsl(var(--popover-foreground))',
+				constructive: {
+					DEFAULT: 'var(--constructive)',
 				},
-				card: {
-					DEFAULT: 'hsl(var(--card))',
-					foreground: 'hsl(var(--card-foreground))',
+				destructive: {
+					DEFAULT: 'var(--destructive)',
 				},
 			},
+			textColor: {
+				primary: {
+					DEFAULT: 'var(--text-primary)',
+				},
+				secondary: {
+					DEFAULT: 'var(--text-secondary)',
+					highlight: 'var(--text-secondary-highlight)',
+				},
+			},
+
 			borderRadius: {
 				lg: `var(--radius)`,
 				md: `calc(var(--radius) - 2px)`,
 				sm: 'calc(var(--radius) - 4px)',
+			},
+			width: {
+				half: '50%',
+			},
+			fontFamily: {
+				sans: ['Geist', 'serif'],
 			},
 		},
 	},

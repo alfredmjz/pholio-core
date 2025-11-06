38fdf116916bb24dde59ba325e2b686c8d83e1b7
Alfred Mah <alfredmjz@gmail.com>
2025-11-05 20:29:44 -0500

    Update configs

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,5 +1,5 @@
-import { createServerClient } from "@supabase/ssr";
-import { NextResponse, type NextRequest } from "next/server";
+import { createServerClient } from '@supabase/ssr';
+import { NextResponse, type NextRequest } from 'next/server';
 
 export async function updateSession(request: NextRequest) {
 	let supabaseResponse = NextResponse.next({
@@ -15,7 +15,7 @@ export async function updateSession(request: NextRequest) {
 					return request.cookies.getAll();
 				},
 				setAll(cookiesToSet) {
-					cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value));
+					cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value));
 					supabaseResponse = NextResponse.next({
 						request,
 					});
@@ -35,10 +35,16 @@ export async function updateSession(request: NextRequest) {
 		data: { user },
 	} = await supabase.auth.getUser();
 
-	if (!user && !request.nextUrl.pathname.startsWith("/login") && !request.nextUrl.pathname.startsWith("/auth")) {
+	if (
+		!user &&
+		!request.nextUrl.pathname.startsWith('/login') &&
+		!request.nextUrl.pathname.startsWith('/auth') &&
+		!request.nextUrl.pathname.startsWith('/error') &&
+		!request.nextUrl.pathname.startsWith('/signup')
+	) {
 		// no user, potentially respond by redirecting the user to the login page
 		const url = request.nextUrl.clone();
-		url.pathname = "/login";
+		url.pathname = '/login';
 		return NextResponse.redirect(url);
 	}
 
@@ -57,4 +63,3 @@ export async function updateSession(request: NextRequest) {
 
 	return supabaseResponse;
 }
-

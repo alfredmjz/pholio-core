6701bb66a4a4a0fdd85510781283e598e4410664
Alfred Mah <alfredmjz@gmail.com>
2025-12-31 14:45:35 -0500

    Migrate to ES256 algo for signing keys

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!;
 
 // Use a global variable to store the Supabase client during development
 // to prevent multiple instances from being created during hot-reloading.
